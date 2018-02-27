e645c41455ec339dcd99c2f9b08e384f62f1f8be
josuperstar <jonathangagnon@gmail.com>
2018-02-27 14:58:58 -0500

    init example for rez test.

--- package.py
+++ package.py
@@ -1,12 +1,26 @@
-name = "requests"
+name = "hello_world_test1"
 
 version = "1.0.0"
 
-authors = ["Jonathan Gagnon"]
+authors = [
+    "ajohns"
+]
+
+description = \
+    """
+    Python-based hello world example package.
+    """
+
+tools = [
+    "hello"
+]
 
 requires = [
-    "python-2.7+"
+    "python"
 ]
+
+uuid = "examples.hello_world_py"
 
 def commands():
     env.PYTHONPATH.append("{root}/python")
+    env.PATH.append("{root}/bin")
